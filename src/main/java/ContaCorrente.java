96ee3c9b5931cbae21b676be96e95737369ecd6e
Fabiano Scorsatto <fabianoscorsatto@gmail.com>
2022-03-14 08:37:37 -0400

    Adicionado métodos basicos

--- src/main/java/ContaCorrente.java
+++ src/main/java/ContaCorrente.java
@@ -3,4 +3,16 @@ public class ContaCorrente {
     public int agencia;
     public int numero;
     public double saldo;
+
+    public void sacar(){
+
+    }
+
+    public void depositar(){
+
+    }
+
+    public void transferir(){
+
+    }
 }
